7fcd3a13905eab2c03f78371364f2c11875fce02
Dokuaob <ndobuobi@gmail.com>
2025-07-15 18:13:02 +0000

    Fix: Move UserProfile.jsx to components folder

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './components/Header.jsx';
 import MainContent from './components/MainContent.jsx';
 import Footer from './components/Footer.jsx';
-import UserProfile from './UserProfile.jsx';
+import UserProfile from './components/UserProfile.jsx';
 
 function App() {
   return (
